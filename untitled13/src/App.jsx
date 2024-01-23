d7ab6d66663131e55a429ba508bd6d41ef67009a
Annknk <145105022+Annknk@users.noreply.github.com>
2024-01-23 19:58:26 +0100

    comit1
    Project start

---
+++ untitled13/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import './App.scss'
+
+function App() {
+
+
+  return (
+    <>
+        <div>
+            <Navbar />
+            <div className="App">
+                <article>
+                    <h1>WAFawfa</h1>
+                </article>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
